3dcbe25b7e483f1a45260b4cd9697df20c61ae4a
morisum <moris@r.sannet.ne.jp>
2024-10-20 09:22:23 +0900

    Initial Commit

---
+++ MyAppForEach1/MyAppForEach1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppForEach1App.swift
+//  MyAppForEach1
+//
+//  Created by MsMacM on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MyAppForEach1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAppForEach1Tests/MyAppForEach1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppForEach1Tests.swift
+//  MyAppForEach1Tests
+//
+//  Created by MsMacM on 2024/10/20.
+//
+
+import Testing
+@testable import MyAppForEach1
+
+struct MyAppForEach1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
